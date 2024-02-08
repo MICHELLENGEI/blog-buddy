6549e4c9253bce72d6e048eab6ad21becad47a75
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-08 15:41:19 +0300

    set up a web server that shares files from folder

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+let initial_path = path.join(__dirname,"public","uploads");
+
+const app = express();
+app.use(express.static(initial_path));
+
+app.get('/',(req, res) => {
+    res.sendFile(path.join(initial_path,"home.html"));
+})
+
+
+app.listen("3000", function() {
+    console.log('listening...........');
+});
+
