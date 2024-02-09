613a0310f2aa9a07b6813319624f937d9fc42a7f
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-09 08:19:19 +0300

    fixed a broken path reference

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const path = require('path');
 
-let initial_path = path.join(__dirname,"public","uploads");
+// Define the path to the 'public' directory
+let initial_path = path.join(__dirname,"public");
 
 const app = express();
+
+// Serve static files from the 'public' directory
 app.use(express.static(initial_path));
 
-app.get('/',(req, res) => {
-    res.sendFile(path.join(initial_path,"home.html"));
-})
-
-
-app.listen("3000", function() {
-    console.log('listening...........');
+// Route to serve the homepage from the 'uploads' folder
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
 });
 
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
