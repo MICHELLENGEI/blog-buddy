6195fea60e26a88220038bc36b557c2adea73b74
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-11 17:50:58 +0300

    added image upload function

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 
+//import file upload package
+const fileupload = require('express-fileupload')
+
 // Define the path to the 'public' directory
 let initial_path = path.join(__dirname,"public");
 
@@ -8,6 +11,7 @@ const app = express();
 
 // Serve static files from the 'public' directory
 app.use(express.static(initial_path));
+app.use(fileupload());
 
 // Route to serve the homepage from the 'uploads' folder
 app.get('/', (req, res) => {
@@ -17,6 +21,28 @@ app.get('/', (req, res) => {
 app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
 });
+
+//uploads route
+app.post('/upload',(req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+
+    //image name
+    let imagename = date.getDate() + date.getTime() + file.name;
+
+    //image upload path
+    let path = 'public/uploads/' + imagename;
+
+    //create upload
+    file.mv(path,(err,result) =>{
+        if(err){
+            throw err;
+        }else{
+            //image upload path
+            res.json('uploads/${imagename}')
+        }
+    })
+})
 
 // Start the server
 app.listen(3000, () => {
