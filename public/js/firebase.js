bdea7a70675f65595bdb84bf083a74aa88a1ae7e
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-12 15:36:09 +0300

    removed ununsed code and comments

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,12 +1,5 @@
- // Import the functions you need from the SDKs you need
- import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
- // TODO: Add SDKs for Firebase products that you want to use
- // https://firebase.google.com/docs/web/setup#available-libraries
 
- // Your web app's Firebase configuration
- // For Firebase JS SDK v7.20.0 and later, measurementId is optional
- const firebaseConfig = {
+ let firebaseConfig = {
    apiKey: "AIzaSyB8rKdHaSlwbF-_sfOllZfCC_R357wEwl0",
    authDomain: "blog-buddy-32ff3.firebaseapp.com",
    projectId: "blog-buddy-32ff3",
@@ -17,7 +10,6 @@
  };
 
  // Initialize Firebase
- const app = initializeApp(firebaseConfig);
- const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
- let db = firebase.firestore();
+let db = firebase.firestore();
