99c72e79151f733cd43964ab607780b1e4cdce4f
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-12 10:04:12 +0300

    added fun to addd blog content to db

--- public/js/editor.js
+++ public/js/editor.js
@@ -12,6 +12,10 @@ bannerImage.addEventListener('change', () => {
     uploadImage(bannerImage, "banner");
 });
 
+uploadInput.addEventListener('change', () => {
+    uploadImage(uploadInput, "image");
+});
+
 const uploadImage = (uploadFile, uploadType) => {
     const [file] = uploadFile.files;
     if (file && file.type.includes("image")) {
@@ -23,8 +27,53 @@ const uploadImage = (uploadFile, uploadType) => {
             body: formdata
         }).then(res => res.json())
             .then(data => {
-                bannerPath = '${location.origin}/${data}';
-                banner.style.backgroundImage = 'url("${bannerPath}")';
+                if (uploadType == "image") {
+                    addImage(data, file.name)
+                } else {
+                    bannerPath = `${location.origin}/${data}`;
+                    banner.style.backgroundImage = `url("${bannerPath}")`;
+                }
+
             })
+    } else {
+        alert("upload Image Only");
     }
 }
+const addImage = (imagepath, alt) => {
+    let curPos = articleField.selectionStart;
+    let textToInsert = `\r![${alt}](${imagepath})\r`;
+    articleField.value = articleField.value.slice(0, curPos) + textToInsert + articleField.value.slice(curPos);
+}
+ 
+let months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+publishBtn.addEventListener('click', () => {
+    if(articleField.value.length && blogTitleField.value.length){
+        //generating Id
+        let letters ='abcdefghijklmnopqrstuvwxyz';
+        let blogTitle = blogTitleField.value.split("").join("-");
+        let id = '';
+        for(let i=0; 1< 4; i++){
+            id += letters[Math.floor(Math.random() * letters.length)];
+        }
+        // setting up doc name
+        let docName =  `&{blogTitle}-${id}`;
+        let date = new Date();
+
+        // access firestore with db variables
+        db.collection("blog").doc(docName).set({
+            title: blogTitleField.value,
+            article:articleField.value,
+            bannerImage:bannerPath,
+            publishedAt:`${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+        })
+        .then(() =>{
+            location.href = `/${docName}`;
+        })
+        .catch((err) =>{
+            console.error(err)
+        })
+
+    }
+    
+})
