4cea4076596ef1f5b8d5934d255ada1866261e8a
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-15 12:46:18 +0300

    added edit display btn

--- public/js/blog.js
+++ public/js/blog.js
@@ -24,6 +24,16 @@ const setupBlog = (data) =>{
 
     publish.innerHTML += ` by ${data.author}`;
    
+   try{
+    if(data.author == auth.currentUser.email.split('@')[0]){
+        let editBtn = document.getElementById('edit-blog-btn');
+        editBtn.style.display = "inline";
+        editBtn.href = `${blogId}/editor`
+    };
+   }catch{
+    
+   }
+
     const article =document.querySelector('.article');
     addArticle(article, data.article);
 }
