95af1b7695fc9a78e5639f86090d585565ec370b
MICHELLLENGEI <michellengei20@gmail.com>
2024-02-15 12:46:56 +0300

    fetch fun for user written blog

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -3,6 +3,8 @@ let ui = new firebaseui.auth.AuthUI(auth);
 
 let login = document.querySelector('.login');
 
+const blogSection = document.querySelector('.blog-section')
+
 auth.onAuthStateChanged((user) => {
     console.log("Auth state changed:", user);
     if(user){
@@ -27,3 +29,36 @@ const setupLoginButton = () => {
     })
 }
 //fetch use written blog
+const getUserWrittenBlogs = () => {
+    db.collection("bligs").where("author", "==", auth.currentUser.email.split('@')[0])
+    .get()
+    .then((blogs) => {
+        blogs.forEach(blog => {
+            createBlog(blog);
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting blogs");
+    })
+}
+const createBlog = (blog) =>{
+    let data = blog.data();
+    blogSection.innerHTML += `
+    <div class="blog-card">
+        <img src="${data.bannerImage}" class="blog-image" alt="">
+        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <a href="/${blog.id}" class="btn dark">read</a>
+        <a href="/${blog.id}/editor" class="btn grey">Edit</a>
+        <a href="#" onClick="deleteBlog('${blog.id})" class="btn danger">Delete</a>
+    </div>
+    `;
+}
+const deleteBlog = (id => {
+    db.collection("blogs").doc(id).delete().then(() => {
+        location.reload();
+    })
+    .catch((error) => {
+        console.log("Error deleting blog")
+    })
+})
